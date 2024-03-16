76aa17c976a614689e569182c221c8c6241c3711
xh7777777 <490327763@qq.com>
2024-03-17 00:00:32 +0800

    fix:url修复

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('NODE_ENV') === 'prd' || env('NODE_ENV') === 'production' ? 'https://background.imxh777.com' : 'http://localhost:1337',
+  url: 'https://background.imxh777.com',
   app: {
     keys: env.array('APP_KEYS'),
   },
